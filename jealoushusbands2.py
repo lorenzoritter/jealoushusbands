e441cec51d417a77fe097dbb0dc9bb78546adb72
lorenzoritter <lorenzo.ritter@gmail.com>
2014-10-29 23:18:00 +0000

    backup before updates

--- jealoushusbands2.py
+++ jealoushusbands2.py
@@ -29,12 +29,19 @@ def isGood(current):                            # people on the same side as the
     good_people = initial.state
     for i in current.state:
         if current.state[i] == current.boat:
-            good_people[i] = 1,
+            good_people[i] = 1
     return good_people
+
+def h(current):
+    result = len(current.state)
+    for i in current.state:
+        result = result - i
+    return result
 
 
 def BFS():
     current = queue.pop(0)
+    print("current: ", current)
     
     good_people = isGood(current)                    # people on the same side as the boat are "good"
     
@@ -42,20 +49,24 @@ def BFS():
        # move = a
         for b in range(0, len(current.state)):
             move = a,b
+            print("move: ", move)
             following = current
             if(good_people[a]==1 and good_people[b]==1):        # check that people on a different side than the boat are not moved
                 counter = 0
                 for i in current.state:
                     if i in move:
                         following.state[i] = alterbit(current.state[i])     # perform the move
-                        if jealousy(following):                         # check if there is jealousy
-                            print("A wife cannot be left with another man unless her husband is present. -> Discarded state ", following)
+                        if following in searched:                           # check if state was already visited
+                            print("break: ", following.state, " already searched")
+                        elif jealousy(following):                         # check if there is jealousy
+                            print("break: ", following.state, " not possible. A wife cannot be left with another man unless her husband is present.")
                             searched.append(following)
-                            return 0
+                            break
                         counter += 1
                 if counter != 0:                                # don't move the boat if no one crossed
                     following.boat = alterbit(current.boat)     # move the boat
-                queue.append(following)
+                    queue.append(following)
+                    print(following.state, " added to queue")
                 
 if __name__ =='__main__':
     noCouples = 3
@@ -65,10 +76,14 @@ if __name__ =='__main__':
     for i in range(0,noCouples):
         initial.state.extend(couple)    # add couples on left side
                                         # the state will be treated as wife1, wife2, wife3, ... husband1, husband2, husband3, ...
-    
-    queue = initial                     # open list
+    queue = []
+    queue.append(initial)                # open list
     searched = []                       # closed list
+    
+    round = 0
              
-    while(queue[0].state != [1,1,1,1,1,1]):
+    while(h(queue[0]) != 0):
+        round = round + 1
+        print("\nRound: ", round)
         BFS()
-    print(queue[0].state)
+    print("Success: ", queue[0].state)
